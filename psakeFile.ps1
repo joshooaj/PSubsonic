31bcb1a54823fa5e5b394fa56b5b3f2da6f5af9e
joshooaj <7669972+joshooaj@users.noreply.github.com>
2025-09-04 22:53:18 -0700

    ci: Get prepared for CI/CD

--- psakeFile.ps1
+++ psakeFile.ps1
@@ -8,3 +8,9 @@ properties {
 task Default -depends Test
 
 task Test -FromModule PowerShellBuild -minimumVersion '0.6.1'
+
+task PublishDocs -depends Build {
+    exec {
+        docker run -v "$($psake.build_script_dir)`:/docs" -e 'CI=true' --entrypoint 'sh' squidfunk/mkdocs-material:latest -c 'pip install -r requirements.txt && mkdocs gh-deploy --force'
+    }
+}
