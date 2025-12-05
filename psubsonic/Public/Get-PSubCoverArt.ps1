56ca5fc623bcd4d41017e5d2a469450613fc7fa5
joshooaj <7669972+joshooaj@users.noreply.github.com>
2025-12-04 21:18:29 -0800

    chore: Accept pipeline input from Get-PSubNowPlaying

--- psubsonic/Public/Get-PSubCoverArt.ps1
+++ psubsonic/Public/Get-PSubCoverArt.ps1
@@ -1,12 +1,37 @@
 function Get-PSubCoverArt {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory, ValueFromPipeline, ParameterSetName = 'NowPlaying')]
+        [ValidateScript({
+            if ($_.coverArt -isnot [string]) {
+                throw ([argumentexception]::new('The supplied parameter does not include a [string] property named "coverArt".', 'InputObject'))
+            } elseif ([string]::IsNullOrEmpty($_.coverArt)) {
+                throw ([argumentexception]::new('The "coverArt" property is empty.', 'InputObject'))
+            }
+            $true
+        })]
+        [object]
+        $InputObject,
+
+        [Parameter(Mandatory, ParameterSetName = 'Id')]
+        [ValidateNotNullOrWhiteSpace()]
         [string]
-        $Id
+        $Id,
+
+        [Parameter()]
+        [string]
+        $Path
     )
 
     process {
+        if ($PSCmdlet.ParameterSetName -eq 'NowPlaying') {
+            $Id = $InputObject.coverArt
+        }
+
+        if ([string]::IsNullOrEmpty($Path)) {
+            $Path = Join-Path ([io.path]::GetTempPath()) ([io.path]::GetRandomFileName() + '.jpg')
+        }
+
         $splat = @{
             Method              = 'getCoverArt'
             ErrorAction         = 'Stop'
@@ -14,10 +39,11 @@ function Get-PSubCoverArt {
                 id = $Id
             }
             AdditionalIrmParams = @{
-                OutFile = Join-Path ([io.path]::GetTempPath()) ([io.path]::GetRandomFileName() + '.jpg')
+                OutFile = $Path
             }
         }
         $null = (invokeApiMethod @splat)
-        $splat.AdditionalIrmParams.OutFile
+
+        $Path
     }
 }
